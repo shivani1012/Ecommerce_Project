73b7aad67f920922ee46e462a25c2c23ef741e79
shivani1012 <Shivanipanchal1012@gmail.com>
2024-02-17 18:03:43 +0530

    step 9 backend steup done

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+app.get('./products', (req, res) => {
+    res.json(products);
+})
+
+app.get('./products/:id', (req, res) => {
+    const product = products.find((prod) => prod._id === req.params.id);
+    res.json(product);
+})
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+})
